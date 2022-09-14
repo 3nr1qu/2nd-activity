407616c8f90df643f07dea4448ce88a69d039f2c
Enrique Siarot <enrique.siarot@student.passerellesnumeriques.org>
2022-09-14 09:27:32 +0800

    add mul

---
+++ multiplication.java
@@ -0,0 +1,9 @@
+public class multiplication{
+    public static void main(String args[]){
+    int num1=20,num2=100,mul;
+    
+    mul=num2*num1;
+    System.out.println("Division of two numbers: "+mul);
+    
+    }
+    }
