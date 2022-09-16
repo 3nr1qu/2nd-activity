0deefdada93c857e8ebf269615d248e10dc890f8
J0rdan-J0r0lan <21103849@usc.edu.ph>
2022-09-16 20:09:08 +0800

    added addition file

---
+++ Add.java
@@ -0,0 +1,27 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasimcalcu;
+
+import java.util.Scanner;
+
+
+public class Add extends Display{
+    int z;
+    Scanner scan = new Scanner (System.in);
+    void display(){
+        
+        System.out.print("Enter First Number: ");
+        int a = scan.nextInt();
+        
+        System.out.print("Enter Second Number: ");
+        int b = scan.nextInt();
+        z= (int) (a + b);
+        
+        System.out.println("Sum is: "+ z);
+       
+    }
+}
