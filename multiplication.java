15bb21b0205ccfadf85d2f356930b7ed4897dc23
J0rdan-J0r0lan <21103849@usc.edu.ph>
2022-09-16 20:02:04 +0800

    remove

--- multiplication.java
+++
@@ -1,9 +0,0 @@
-public class multiplication{
-    public static void main(String args[]){
-    int num1=20,num2=100,mul;
-    
-    mul=num2*num1;
-    System.out.println("Division of two numbers: "+mul);
-    
-    }
-    }
