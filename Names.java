20f48445c52c396c4c586157bf2b1ab0a510119d
3nr1qu <enrique.siarot@student.passerellesnumeriques.org>
2022-09-17 13:53:58 +0800

    add

---
+++ Names.java
@@ -0,0 +1,8 @@
+public class Names {
+
+    public static void main(String[] args) {
+        System.out.println("Enrique Siarot aka Gwapo");
+        System.out.println("Jordan Jorolan aka Tisoy");
+    }
+
+}
