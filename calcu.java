a169adcade18112049b8d7ff4daa035aa35fadfa
3nr1qu <enrique.siarot@student.passerellesnumeriques.org>
2022-09-17 13:06:57 +0800

    edited

--- calcu.java
+++ calcu.java
@@ -1,16 +1,7 @@
 
 
-    public static void display(){
-        
-        System.out.print("Enter First Number: ");
-        int a = scan.nextInt();
-        
-        System.out.print("Enter Second Number: ");
-        int b = scan.nextInt();
-        int z= (int) (a + b);
-        
-        System.out.println("Sum is: "+ z);
-       
-    }
-    
+public static int add(int x, int y)
+{
+    int result = x + y;
+    return result;
 }
