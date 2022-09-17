ce58492389126ab92ff87559940aa8d5fd5dcce0
3nr1qu <enrique.siarot@student.passerellesnumeriques.org>
2022-09-17 13:10:10 +0800

    edited

--- calcu.java
+++ calcu.java
@@ -1,25 +1,5 @@
-package javasimcalcu;
-
-import java.util.Scanner;
-
-/**
- *
- * @author 2ndyrGroupA
- */
-public class Division extends Display{
-    float z;
-    Scanner scan = new Scanner (System.in);
-    void display(){
-        
-        System.out.print("Enter First Number: ");
-        float a = scan.nextInt();
-        
-        System.out.print("Enter Second Number: ");
-        float b = scan.nextInt();
-        z= (float) (a / b);
-        
-        System.out.println("Division is: "+ z);
-       
+public static int div(int x, int y)
+    {
+        int result = x/y;
+        return result;
     }
-
-}
